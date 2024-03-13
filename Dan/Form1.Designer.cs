70f86455f8a1d0eebe31dc3ca39aa53c588d09e5
hzcho <ubelwertyas@gmail.com>
2024-03-13 06:29:13 +0500

    Добавьте файлы проекта.

---
+++ Dan/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Dan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxAloneGraphs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelAlfa = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(578, 255);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(365, 226);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(578, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(365, 237);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(949, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(352, 469);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 121);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(371, 139);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 110);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(371, 255);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(201, 110);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // textBoxAloneGraphs
+            // 
+            this.textBoxAloneGraphs.Location = new System.Drawing.Point(371, 371);
+            this.textBoxAloneGraphs.Multiline = true;
+            this.textBoxAloneGraphs.Name = "textBoxAloneGraphs";
+            this.textBoxAloneGraphs.Size = new System.Drawing.Size(201, 110);
+            this.textBoxAloneGraphs.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(265, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 120);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label1";
+            // 
+            // labelAlfa
+            // 
+            this.labelAlfa.Location = new System.Drawing.Point(265, 255);
+            this.labelAlfa.Name = "labelAlfa";
+            this.labelAlfa.Size = new System.Drawing.Size(100, 120);
+            this.labelAlfa.TabIndex = 8;
+            this.labelAlfa.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "button2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1313, 498);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelAlfa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAloneGraphs);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxAloneGraphs;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelAlfa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Dan/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Dan
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<LevelGraphs> tree;
+        LevelGraphs prevLevelGraphs;
+        List<LevelGraphs> aloneGraphs;
+        List<Alfas> alfas;
+        int numLevel = 1;
+        int count = 2;
+        int countAloneGraphs = 0;
+        int m = 0;
+        int N = 0;
+        bool isEnded = false;
+        bool isTreeComplete = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("", "Все вершины");
+            dataGridView1.Columns.Add("", "Висячие вершины");
+            dataGridView2.Columns.Add("", "№");
+            dataGridView2.Columns.Add("", "Число вершин");
+            dataGridView2.Columns.Add("", "Число висячих вершин");
+            dataGridView2.Columns.Add("", "Альфа");
+            dataGridView2.Columns.Add("", "Глубина");
+
+            chart1.Series[0].ChartType = SeriesChartType.Column;
+            chart1.Series[0].IsVisibleInLegend = false;
+
+            Axis ax = new Axis();
+            ax.Title = "m";
+            chart1.ChartAreas[0].AxisX = ax;
+
+            Axis ay = new Axis();
+            ay.Title = "P";
+            chart1.ChartAreas[0].AxisY = ay;
+        }
+
+        int countRandomEdge = 0;
+        bool createOneTree = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isEnded = false;
+            isTreeComplete = false;
+            createOneTree = true;
+            countRandomEdge = 0;
+            dataGridView1.Rows.Clear();
+            chart1.Series[0].Points.Clear();
+
+            m = Convert.ToInt32(textBox1.Text);
+            N = Convert.ToInt32(textBox2.Text);
+            numLevel = 1;
+            count = 2;
+            countAloneGraphs = 0;
+            tree = new List<LevelGraphs>();
+            aloneGraphs = new List<LevelGraphs>();
+
+            Graph graph = new Graph();
+            LevelGraphs levelGraphs = new LevelGraphs();
+            levelGraphs.numLevel = numLevel++;
+            graph.Num = 1;
+            graph.Parent = 0;
+            levelGraphs.graphs.Add(graph);
+            prevLevelGraphs = levelGraphs;
+            tree.Add(levelGraphs);
+
+            while (count <= N && !isEnded)
+            {
+                CreateTree();
+            }
+
+            LastLevelAloneTree();
+
+            int rowIndex = 0;
+
+            textBoxResult.Text = "";
+
+            for (int i = 0; i < tree.Count; i++)
+            {
+                LevelGraphs level = tree[i];
+                textBoxResult.AppendText(level.numLevel + ": ");
+                for (int j = 0; j < level.graphs.Count; j++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowIndex++].Cells[0].Value = level.graphs[j].Num + "-" + level.graphs[j].Parent;
+                    textBoxResult.AppendText(level.graphs[j].Num + "-" + level.graphs[j].Parent + ", ");
+                }
+                textBoxResult.AppendText("\n");
+            }
+            textBoxResult.AppendText("\n\n");
+
+            rowIndex = 0;
+            textBoxAloneGraphs.Text = "";
+            for (int i = 0; i < aloneGraphs.Count; i++)
+            {
+                LevelGraphs level = aloneGraphs[i];
+                textBoxAloneGraphs.AppendText(level.numLevel + ": ");
+                for (int j = 0; j < level.graphs.Count; j++)
+                {
+                    dataGridView1.Rows[rowIndex++].Cells[1].Value = level.graphs[j].Num + "-" + level.graphs[j].Parent;
+                    textBoxAloneGraphs.AppendText(level.graphs[j].Num + "-" + level.graphs[j].Parent + ", ");
+                }
+                textBoxAloneGraphs.AppendText("\n");
+            }
+            textBoxAloneGraphs.AppendText("\n\n");
+
+            label4.Text = "Висячие вершины- " + countAloneGraphs;
+            double alfa = (count - 1.0) / countAloneGraphs;
+            labelAlfa.Text = "Alfa = " + Math.Round(alfa, 3);
+
+            createOneTree = false;
+            button1.BackColor = Color.Bisque;
+            button6.BackColor = Color.Transparent;
+        }
+
+        public void CreateTree()
+        {
+            LevelGraphs levelGraphs = new LevelGraphs();
+            LevelGraphs alonelevelGraphs = new LevelGraphs();
+            alonelevelGraphs.numLevel = numLevel - 1;
+            levelGraphs.numLevel = numLevel++;
+            int k = prevLevelGraphs.graphs.Count;
+            isEnded = true;
+
+            for (int i = 0; i < k; i++)
+            {
+                int numPar = prevLevelGraphs.graphs[i].Num;
+                int ran;
+                if (isDetermTree)
+                {
+                    ran = m;
+                }
+                else
+                {
+                    if (numPar == 1)
+                    {
+                        ran = random.Next(2, m);
+                    }
+                    else
+                    {
+                        ran = random.Next(m);
+                    }
+                }
+
+                if (createOneTree)
+                {
+                    chart1.Series[0].Points.AddXY(ran, ++countRandomEdge);
+                }
+
+                if (isTreeComplete) ran = 0;
+
+                if (ran != 0) isEnded = false;
+
+                if (ran == 0)
+                {
+                    Graph aloneGraph = new Graph();
+                    aloneGraph.Num = numPar;
+                    aloneGraph.Parent = prevLevelGraphs.graphs[i].Parent;
+                    alonelevelGraphs.graphs.Add(aloneGraph);
+                    countAloneGraphs++;
+                }
+
+                if (!isTreeComplete)
+                {
+                    for (int j = 0; j < ran; j++)
+                    {
+                        Graph graph = new Graph();
+                        graph.Num = count++;
+                        graph.Parent = numPar;
+                        levelGraphs.graphs.Add(graph);
+                        if (count > N)
+                        {
+                            isTreeComplete = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            prevLevelGraphs = levelGraphs;
+            tree.Add(levelGraphs);
+            aloneGraphs.Add(alonelevelGraphs);
+        }
+
+        public void LastLevelAloneTree()
+        {
+            LevelGraphs levelGraphs = new LevelGraphs();
+            LevelGraphs alonelevelGraphs = new LevelGraphs();
+            alonelevelGraphs.numLevel = numLevel - 1;
+            levelGraphs.numLevel = numLevel++;
+            int k = prevLevelGraphs.graphs.Count;
+            isEnded = true;
+
+            for (int i = 0; i < k; i++)
+            {
+                int numPar = prevLevelGraphs.graphs[i].Num;
+                Graph aloneGraph = new Graph();
+                aloneGraph.Num = numPar;
+                aloneGraph.Parent = prevLevelGraphs.graphs[i].Parent;
+                alonelevelGraphs.graphs.Add(aloneGraph);
+                countAloneGraphs++;
+            }
+            aloneGraphs.Add(alonelevelGraphs);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            isDetermTree = true;
+            createOneTree = true;
+            countRandomEdge = 0;
+            dataGridView1.Rows.Clear();
+            chart1.Series[0].Points.Clear();
+            isEnded = false;
+            isTreeComplete = false;
+
+            m = Convert.ToInt32(textBox1.Text);
+            N = Convert.ToInt32(textBox2.Text);
+            numLevel = 1;
+            count = 2;
+            countAloneGraphs = 0;
+            tree = new List<LevelGraphs>();
+            aloneGraphs = new List<LevelGraphs>();
+
+            Graph graph = new Graph();
+            LevelGraphs levelGraphs = new LevelGraphs();
+            levelGraphs.numLevel = numLevel++;
+            graph.Num = 1;
+            graph.Parent = 0;
+            levelGraphs.graphs.Add(graph);
+            prevLevelGraphs = levelGraphs;
+            tree.Add(levelGraphs);
+
+            while (count <= N && !isEnded)
+            {
+                CreateTree();
+            }
+
+            LastLevelAloneTree();
+
+            int rowIndex = 0;
+            textBoxResult.Text = "";
+
+            for (int i = 0; i < tree.Count; i++)
+            {
+                LevelGraphs level = tree[i];
+                textBoxResult.AppendText(level.numLevel + ": ");
+                for (int j = 0; j < level.graphs.Count; j++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowIndex++].Cells[0].Value = level.graphs[j].Num + "-" + level.graphs[j].Parent;
+                    textBoxResult.AppendText(level.graphs[j].Num + "-" + level.graphs[j].Parent + ", ");
+                }
+                textBoxResult.AppendText("\n");
+            }
+            textBoxResult.AppendText("\n\n");
+
+            rowIndex = 0;
+            textBoxAloneGraphs.Text = "";
+            for (int i = 0; i < aloneGraphs.Count; i++)
+            {
+                LevelGraphs level = aloneGraphs[i];
+                textBoxAloneGraphs.AppendText(level.numLevel + ": ");
+                for (int j = 0; j < level.graphs.Count; j++)
+                {
+                    dataGridView1.Rows[rowIndex++].Cells[1].Value = level.graphs[j].Num + "-" + level.graphs[j].Parent;
+                    textBoxAloneGraphs.AppendText(level.graphs[j].Num + "-" + level.graphs[j].Parent + ", ");
+                }
+                textBoxAloneGraphs.AppendText("\n");
+            }
+            textBoxAloneGraphs.AppendText("\n\n");
+
+            label4.Text = "Висячие вершины- " + countAloneGraphs;
+            double alfa = (count - 1.0) / countAloneGraphs;
+            labelAlfa.Text = "Alfa = " + Math.Round(alfa, 3);
+
+            createOneTree = false;
+
+            button6.BackColor = Color.Bisque;
+            button1.BackColor = Color.Transparent;
+        }
+
+        bool isDetermTree = false;
+
+        public class LevelGraphs
+        {
+            public int numLevel;
+            public List<Graph> graphs = new List<Graph>();
+        }
+
+        public class Graph
+        {
+            public int Num { get; set; }
+            public int Parent { get; set; }
+            public Vertex V { get; set; }
+        }
+
+        public class Alfas
+        {
+            public double Alfa { get; set; }
+            public int CountVertex { get; set; }
+        }
+
+        public class Vertex
+        {
+            // vertex properties
+        }
+    }
+
+}
